e31359e8506dbf6a57a31a412938b3a4ffc33bf1
Luis Clar Prohens <luisclar@MacBook-Pro-de-Luis.local>
2021-02-23 17:23:41 +0100

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './views/home/home.component';
+import { PostComponent } from './views/post/post.component';
+import { CommentComponent } from './views/comment/comment.component';
+import { CategoriesComponent } from './views/categories/categories.component';
+import { MedalsComponent } from './views/medals/medals.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PostComponent,
+    CommentComponent,
+    CategoriesComponent,
+    MedalsComponent
   ],
   imports: [
     BrowserModule,
